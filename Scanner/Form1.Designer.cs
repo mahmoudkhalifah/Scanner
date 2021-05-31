6503a425c9f15980eb34e13420122895a2bd10a5
Mahmoud khalifa <57409035+mahmoudkhalifah@users.noreply.github.com>
2021-05-31 20:31:23 +0200

    Add project files.

---
+++ Scanner/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿
+using System;
+
+namespace Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.codeBox = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.compileBtn = new System.Windows.Forms.Button();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.newBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // codeBox
+            // 
+            this.codeBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(36)))));
+            this.codeBox.ForeColor = System.Drawing.Color.White;
+            this.codeBox.Location = new System.Drawing.Point(12, 12);
+            this.codeBox.Name = "codeBox";
+            this.codeBox.Size = new System.Drawing.Size(811, 522);
+            this.codeBox.TabIndex = 0;
+            this.codeBox.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // compileBtn
+            // 
+            this.compileBtn.Location = new System.Drawing.Point(866, 68);
+            this.compileBtn.Name = "compileBtn";
+            this.compileBtn.Size = new System.Drawing.Size(94, 29);
+            this.compileBtn.TabIndex = 1;
+            this.compileBtn.Text = "Compile";
+            this.compileBtn.UseVisualStyleBackColor = true;
+            this.compileBtn.Click += new System.EventHandler(this.compileBtn_Click);
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Location = new System.Drawing.Point(866, 113);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(94, 29);
+            this.browseBtn.TabIndex = 2;
+            this.browseBtn.Text = "Browse";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // newBtn
+            // 
+            this.newBtn.Location = new System.Drawing.Point(866, 23);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(94, 29);
+            this.newBtn.TabIndex = 3;
+            this.newBtn.Text = "New";
+            this.newBtn.UseVisualStyleBackColor = true;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(866, 160);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(94, 29);
+            this.saveBtn.TabIndex = 4;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(998, 546);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.newBtn);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.compileBtn);
+            this.Controls.Add(this.codeBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox codeBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button compileBtn;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+
---
+++ Scanner/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scanner
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd = new OpenFileDialog
+        {
+            Filter="txt files (*.txt)|*.txt",
+        };
+
+        string code = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void compileBtn_Click(object sender, EventArgs e)
+        {
+            code = codeBox.Text;
+            System.Diagnostics.Debug.WriteLine(code);
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                code = sr.ReadToEnd();
+                System.Diagnostics.Debug.WriteLine(code);
+                sr.Close();
+            }
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            codeBox.Text = "";
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "txt files (*.txt)|*.txt";
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                StreamWriter sr = new StreamWriter(sfd.FileName);
+                sr.Write(codeBox.Text);
+                sr.Close();
+            }
+        }
+    }
+}
